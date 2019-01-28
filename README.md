1a24e6ed2c3b73828b45a1e3d105363f53060251
Faruk Parhat <faruh.paerhati@gmail.com>
2019-01-28 23:43:37 +0900

    Added readme and initial configuration file

---
+++ README.md
@@ -0,0 +1,17 @@
+# Windows Powershell Configuration File
+## Installation
+
+Clone into your home directory
+
+    git clone https://github.com/Paerhati/psconfig.git ~\psconfig
+
+(Optional) Create your Powershell Directory
+
+    mkdir ~\Documents\WindowsPowerShell
+
+(Optional) Point your profile to the cloned one
+
+    echo ". ~\psconfig\profile.ps1" >> $profile
+
+Restart Powershell
+
---
+++ profile.ps1
@@ -0,0 +1,107 @@
+﻿# --- Chocolatey Profile ---
+
+$ChocolateyProfile = "$env:ChocolateyInstall\helpers\chocolateyProfile.psm1"
+if (Test-Path($ChocolateyProfile)) {
+    Import-Module "$ChocolateyProfile"
+}
+
+# --- Git ---
+
+function gdiff
+{
+    git diff
+}
+
+function gstatus
+{
+    git status
+}
+
+function gpull
+{
+    git pull
+}
+
+function gpush
+{
+    git push
+}
+
+function gclone ($repo)
+{
+    if (!$repo)
+    {
+        Write-Output "Please enter a repository path"
+    }
+    else
+    {
+        git clone git@github.com:$repo
+    }
+}
+
+function gcommit-and-push ($message)
+{
+    if (!$message)
+    {
+        Write-Output "Please enter a commit message"
+    }
+    else
+    {
+        git commit -am $message; git push
+    }
+}
+
+# --- Fast Edits ---
+
+function evim
+{
+    e ~\.vim\.vimrc
+}
+
+function epp
+{
+    e $PROFILE
+}
+
+function e ($file)
+{
+    if (!$file)
+    {
+        Write-Output "Please enter a file path"
+    }
+    else
+    {
+        gvim --remote $file
+    }
+}
+
+# --- Directory Commands ---
+
+function ws
+{
+    cd c:\Workspace
+}
+
+function repos
+{
+    cd c:\Workspace\Repositories
+}
+
+function temp
+{
+    cd c:\temp
+}
+
+function home
+{
+    cd ~
+}
+
+# --- Initialization ---
+
+function init
+{
+    Set-PSReadlineOption -BellStyle None
+}
+
+init
